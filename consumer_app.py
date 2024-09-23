0cf9438d3b8d5cba501c5a21b8f0473fa31e6ac1
Surajjadhav05 <103742548+Surajjadhav05@users.noreply.github.com>
2024-09-23 13:03:16 +0530

    Update consumer_app.py

--- consumer_app.py
+++ consumer_app.py
@@ -142,9 +142,10 @@ if consume:
                                     st.table(df_f)
                         
                         if features==1:
-                            response1 = requests.post("https://172.16.20.71:8003/api/graph/clearcache",verify=False)
+                            
                             prediction_placeholder=st.empty()
                             try:
+                                response1 = requests.post("https://172.16.20.71:8003/api/graph/clearcache",verify=False)
                                 response = requests.post("https://172.16.20.71:8004/api/predictor/predict",verify=False)
                                 
                                 with prediction_placeholder.container():
